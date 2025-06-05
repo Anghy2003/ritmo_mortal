5bfd8857d90fb498b5542bafcca412edaf3a5363
Andrea <mercedes.illescas.est@tecazuay.edu.ec>
2025-06-04 23:39:30 -0500

    sensor

--- lib/ui/prueba.dart
+++ lib/ui/prueba.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proximity_sensor/proximity_sensor.dart';
+import 'dart:async';
 
 class ProximityDemo extends StatefulWidget {
   @override
@@ -8,15 +9,34 @@ class ProximityDemo extends StatefulWidget {
 
 class _ProximityDemoState extends State<ProximityDemo> {
   bool _isNear = false;
+  bool _hasSensor = true;
+  StreamSubscription<int>? _subscription;
 
   @override
   void initState() {
     super.initState();
-    ProximitySensor.events.listen((int event) {
-      setState(() {
-        _isNear = event > 0; 
+    _startListening();
+  }
+
+  void _startListening() {
+    try {
+      _subscription = ProximitySensor.events.listen((int event) {
+        setState(() {
+          _isNear = event > 0;
+        });
       });
-    });
+    } catch (e) {
+      setState(() {
+        _hasSensor = false;
+      });
+      print("Error verificando el sensor de proximidad: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
   }
 
   @override
@@ -24,10 +44,15 @@ class _ProximityDemoState extends State<ProximityDemo> {
     return Scaffold(
       appBar: AppBar(title: const Text("Proximity Sensor Demo")),
       body: Center(
-        child: Text(
-          _isNear ? "Cerca del sensor" : "Lejos del sensor",
-          style: const TextStyle(fontSize: 24),
-        ),
+        child: _hasSensor
+            ? Text(
+                _isNear ? "Cerca del sensor" : "Lejos del sensor",
+                style: const TextStyle(fontSize: 24),
+              )
+            : const Text(
+                "Este dispositivo no tiene un sensor de proximidad.",
+                style: TextStyle(fontSize: 18, color: Colors.red),
+              ),
       ),
     );
   }
